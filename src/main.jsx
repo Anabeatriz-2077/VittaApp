a3b7d84e68176bb1d088ea87cd51d1f2a072ed98
Ana Beatriz <anacarvahlo2077@gmail.com>
2026-05-20 23:17:33 -0300

    Responsável por iniciar a aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,35 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import "./styles/global.css";
+
+import { BrowserRouter } from "react-router-dom";
+
+import { AuthProvider } from "./context/AuthContext";
+import { FavoritesProvider } from "./context/FavoritesContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    
+    {/* BrowserRouter permite usar rotas */}
+    <BrowserRouter>
+
+      {/* Context API de login */}
+      <AuthProvider>
+
+        {/* Context API de favoritos */}
+        <FavoritesProvider>
+
+          <App />
+
+        </FavoritesProvider>
+      </AuthProvider>
+
+    </BrowserRouter>
+  </React.StrictMode>
+);
