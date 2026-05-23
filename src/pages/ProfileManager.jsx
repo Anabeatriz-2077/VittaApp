e9dbbf59c174438950dd3179572abd967176656e
Ana Beatriz <anacarvahlo2077@gmail.com>
2026-05-22 22:37:42 -0300

    Implementa página de mensagens

---
+++ src/pages/ProfileManager.jsx
@@ -0,0 +1,18 @@
+import MessageBox from "../../components/MessageBox/MessageBox";
+
+function Messages() {
+
+  return (
+
+    <div>
+
+      <h1>Mensagens</h1>
+
+      <MessageBox />
+
+    </div>
+
+  );
+}
+
+export default Messages;
