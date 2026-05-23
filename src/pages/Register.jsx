2051b06d224d8c9bac8c5a93356d7e9846b03c61
Ana Beatriz <anacarvahlo2077@gmail.com>
2026-05-22 22:36:48 -0300

    Cria página de cadastro

---
+++ src/pages/Register.jsx
@@ -0,0 +1,37 @@
+function Register() {
+
+  return (
+
+    <div>
+
+      <h1>Cadastro</h1>
+
+      <form>
+
+        <input
+          type="text"
+          placeholder="Nome"
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+        />
+
+        <input
+          type="password"
+          placeholder="Senha"
+        />
+
+        <button>
+          Cadastrar
+        </button>
+
+      </form>
+
+    </div>
+
+  );
+}
+
+export default Register;
