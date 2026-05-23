68e89aaafde01cdad715e114aa3f23528283328e
Ana Beatriz <anacarvahlo2077@gmail.com>
2026-05-22 22:34:20 -0300

    Adiciona componente de loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,18 @@
+import "./Loading.css";
+
+function Loading() {
+
+  return (
+
+    <div className="loading-container">
+
+      <div className="spinner"></div>
+
+      <p>Carregando...</p>
+
+    </div>
+
+  );
+}
+
+export default Loading;
