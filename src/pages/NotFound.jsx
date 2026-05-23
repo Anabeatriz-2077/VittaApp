5a5831343a5e4473fef4d1b0dd27a38fc0c68c12
Ana Beatriz <anacarvahlo2077@gmail.com>
2026-05-22 22:39:29 -0300

    Adiciona página 404

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,20 @@
+import BackButton from "../../components/BackButton/BackButton";
+
+function NotFound() {
+
+  return (
+
+    <div>
+
+      <h1>404</h1>
+
+      <p>Página não encontrada.</p>
+
+      <BackButton />
+
+    </div>
+
+  );
+}
+
+export default NotFound;
